1acd27c309b2bc13772e652680c175d5f61ddd75
Brianna Doubt <bri@briannadoubt.com>
2021-10-22 17:38:45 -0700

    Set up project

--- Package.swift
+++ Package.swift
@@ -1,28 +1,25 @@
-// swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.3
 
 import PackageDescription
 
 let package = Package(
     name: "DeviceRotation",
+    platforms: [.iOS(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "DeviceRotation",
-            targets: ["DeviceRotation"]),
+            targets: ["DeviceRotation"]
+        )
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "DeviceRotation",
-            dependencies: []),
+            dependencies: []
+        ),
         .testTarget(
             name: "DeviceRotationTests",
-            dependencies: ["DeviceRotation"]),
+            dependencies: ["DeviceRotation"]
+        )
     ]
 )
--- Sources/DeviceRotation/DeviceRotation.swift
+++ Sources/DeviceRotation/DeviceRotation.swift
@@ -1,6 +1,21 @@
-public struct DeviceRotation {
-    public private(set) var text = "Hello, World!"
 
-    public init() {
+import SwiftUI
+
+public struct DeviceRotationViewModifier: ViewModifier {
+    
+    public let action: (UIDeviceOrientation) -> Void
+
+    public func body(content: Content) -> some View {
+        content
+            .onAppear()
+            .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
+                action(UIDevice.current.orientation)
+            }
+    }
+}
+
+public extension View {
+    func onRotate(perform action: @escaping (UIDeviceOrientation) -> Void) -> some View {
+        self.modifier(DeviceRotationViewModifier(action: action))
     }
 }
